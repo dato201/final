14443757093dbf7f9131dfe077aea2be445acdc6
dato201 <146935809+dato201@users.noreply.github.com>
2023-11-06 21:19:00 +0400

    add index dan

---
+++ script.js
@@ -0,0 +1,19 @@
+let headerNav =  document.querySelector(".header_nav");
+let burger = document.querySelector("#burger");
+let porm = document.querySelector(".porm");
+let xmark = document.querySelector(".xmark")
+
+burger.addEventListener("click", ()=>{
+    headerNav.style.display = "block";
+    porm.style.display = "flex";
+    xmark.style.display = "block";
+    burger.style.display = "none";
+    
+});
+
+xmark.addEventListener("click", ()=>{
+    headerNav.style.display = "none";
+    porm.style.display = "none";
+    xmark.style.display = "none";
+    burger.style.display = "block";
+})
